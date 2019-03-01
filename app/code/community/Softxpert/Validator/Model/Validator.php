463217bff1e3093eb4cc6dde5b0a783abe8543df
Mohamed Abdul-Fattah <csmohamed8@gmail.com>
2019-03-01 15:00:43 +0200

    Initialize Softxpert_Validator Model

---
+++ app/code/community/Softxpert/Validator/Model/Validator.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Softxpert_Validator extension
+ *
+ * @category    Softxpert
+ * @package     Softxpert_Validator
+ * @copyright   Copyright (c) 2019
+ * @author      Mohamed Abdul-Fattah <csmohamed8@gmail.com>
+ */
+
+class Softxpert_Validator_Model_Validator
+{
+    //
+}
